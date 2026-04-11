74bcc0d101f851870583d4f7bcf0c95b6be21530
M2026a <texceed8074@yahoo.co.jp>
2026-04-11 23:20:03 +0900

    Add files via upload

---
+++ Photo_Finder/start_Photo_Finder.bat
@@ -0,0 +1,21 @@
+@echo off
+cd /d "%~dp0"
+echo ==============================================
+echo   Photo_Finder
+echo ==============================================
+
+echo [1/3] Installing requirements...
+py -m pip install -r requirements.txt
+if errorlevel 1 pause & exit /b 1
+
+echo [2/3] Running...
+py app\main.py
+if errorlevel 1 pause & exit /b 1
+
+echo [3/3] Opening dashboard...
+start "" "%~dp0output\index.html"
+
+pause
+
+start "" output\index.html
+pause

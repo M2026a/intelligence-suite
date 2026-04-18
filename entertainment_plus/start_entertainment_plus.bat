a748cc5e625572fbf2b40cdf38f489e1976318ba
M2026a <texceed8074@yahoo.co.jp>
2026-04-18 11:54:08 +0900

    Add files via upload

--- entertainment_plus/start_entertainment_plus.bat
+++ entertainment_plus/start_entertainment_plus.bat
@@ -1,14 +1,20 @@
 @echo off
+chcp 65001 > nul
+set PYTHONIOENCODING=utf-8
 cd /d "%~dp0"
 echo ==============================================
-echo   Entertainment +
+echo   entertainment_plus
 echo ==============================================
+
 echo [1/3] Installing requirements...
 py -m pip install -r requirements.txt
 if errorlevel 1 pause & exit /b 1
+
 echo [2/3] Running...
 py app\main.py
 if errorlevel 1 pause & exit /b 1
+
 echo [3/3] Opening dashboard...
 start "" "%~dp0output\index.html"
+
 pause

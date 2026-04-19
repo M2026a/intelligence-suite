1bf63469b11e056eac71e0803de6742d2b9a4d50
M2026a <texceed8074@yahoo.co.jp>
2026-04-19 14:02:12 +0900

    Add files via upload

--- generate_index.py
+++ generate_index.py
@@ -205,6 +205,9 @@ def build_html(groups: List[Dict[str, Any]], update_time: str) -> str:
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{SITE_TITLE}</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 
 <style>
 body {{
